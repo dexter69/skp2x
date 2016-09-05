14bb7a59b74b3b2f9b9992a97e8b9ca42a432cd3
dexter <dexter@work>
2016-09-05 09:54:23 +0200

    Testy openssl

---
+++ app/Skrypty/test.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo (extension_loaded('openssl')?'SSL loaded':'SSL not loaded')."\n";
