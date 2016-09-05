21dbcf7891aa83f8a8cb15dcc4745735dff82198
dexter <dexter@work>
2016-09-05 10:12:46 +0200

    Tak, coś źle jest skonfigurowane na serwerze, jezeli chodzi o SSL. Na razie zostawiam nie szyfrowane

--- app/Skrypty/class.poczta.php
+++ app/Skrypty/class.poczta.php
@@ -10,14 +10,14 @@ class POCZTA {
     public function setMainEmailParams( $mail, $config ) { // $config - array z parametrami smtp
         // zakładamy, że $email to obiket PHPMailer'a
         $mail->isSMTP(); 
-        $mail->SMTPDebug  = 3;
+        //$mail->SMTPDebug  = 3;
         $mail->Timeout  =   20;
         $mail->CharSet = 'UTF-8';
         $mail->Host = $config['host'];  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
         $mail->Username = $config['username'];                 // SMTP username
-        $mail->Password = $config['password'];                           // SMTP password
-        //$mail->SMTPSecure = 'tls';                            // Nie chce na razie szyfrowania //Enable TLS encryption, `ssl` also accepted
+        $mail->Password = $config['password'];                  // SMTP password
+        //$mail->SMTPSecure = 'tls';                            //Enable TLS encryption, `ssl` also accepted
         $mail->SMTPAutoTLS = false;                             //wyłącza auto tls
         $mail->Port = 587;                                   // TCP port to connect to
         //$mail->setFrom('skp@polskiekarty.pl', 'SKP');
