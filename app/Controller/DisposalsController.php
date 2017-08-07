6b42abf4294118452476839255e950d6ff29c7f2
dexter69 <dex@polskiekarty.pl>
2017-08-07 14:10:18 +0200

    Code cleaning

--- app/Controller/DisposalsController.php
+++ app/Controller/DisposalsController.php
@@ -32,18 +32,4 @@ class DisposalsController extends AppController {
         
     }
 
-    public function searchx() {
-
-        // Zapisujemy w modelu otrzymane
-        $this->Disposal->otrzymane = $this->request->data;
-        $x = $this->Disposal->searchParams;
-        $data = $this->Disposal->theSearch();        
-        $this->Paginator->settings = $y = $this->paginate = $this->Disposal->searchParams;        
-        $disposals = null;
-        $disposals = $this->Paginator->paginate( 'Disposal');
-        array_unshift($data, $this->request->data);
-        $this->layout='ajax';
-        $this->set( compact( ['data', 'x', 'y', 'disposals']) ); 
-    }
-
 }
