62f8ce59c747b318de25d7cb50621f8aaa7e0d3d
dexter69 <dex@polskiekarty.pl>
2018-06-06 13:15:14 +0200

    Sensowniej zrobione kody dla klienta Agnieszki

---
+++ app/Skrypty/kody/Agnieszka/funkcje.php
@@ -0,0 +1,29 @@
+<?php
+
+define( "FOLDER", "bazy");
+
+// Wczytaj kody z pliku o nazwie w $nazwa, zwróć w formie tablicy
+function wczytaj( $nazwa ) {
+
+    $h = fopen( "./" . FOLDER . "/" . $nazwa ,"r");
+    if( $h ) {
+        $out = [];
+        while( $s = fgets($h)) {
+            $out[] = trim($s);
+        }
+        fclose($h);
+        return $out;
+    }
+    return ["GIBON"];
+}
+
+function zasysnij( $arrTab = [] ) {
+
+    $ret = [];
+    if( !empty($arrTab) ) {        
+        foreach( $arrTab as $name ) {
+            $ret = array_merge( $ret, wczytaj($name) );            
+        }
+    }
+    return $ret;
+}
---
+++ app/Skrypty/kody/Agnieszka/kody.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Klient Agnieszki:
+http://skp.lan/customers/view/242
+Raz na jakiś czas życzy sobie wygenrowanie kodów o następujących parametrach:
+LLLLCC
+gdzie L - duża litera, C - cyfra,
+np. OEYL54
+Generowane kody muszą być unikalne globalnie, czyli rózne od jakichkolwiek do tej pory wygenerowanych.
+Czyli różne od kodów poprzednich bazach i w bazie bieżącej */
+
+require 'funkcje.php';
+
+// To są stare bazy, wszystkie bazy - klienta i te, które robiliśmy my
+$arrOfFiles = [
+    'oldest.txt', // klienta
+    'kody40k.txt',
+    'kody30k.txt',
+    'kody30k_cze-2018.txt' // baza wygenerowana 6.06.2018
+];
+
+// Plik z wygenerowanymi kodami, sprawdzamy czy się nie powtarzają z tym co już mamy
+$newF = 'new.txt';
+
+// Plik w którym zapisujemy wynik
+$outF = 'res.txt';
+
+/*
+- Tworzymy tablicę $old, do której wczytujemy wszystkie stare kody, z nią będziemy sprawdzać nowe
+- Nowe kody wczytujemy do tablicy $new
+- Tworzymy tablicę $out, do której będzemy wpisywać nowe, sprwdzone kody
+- Algorytm
+Weź kod z $new, sprawdź czy nie ma go w $old i $out. Jezeli nie, to dodaj do $out
+Zrób tak dla wszystkich
+*/
+
+
+
+$old = zasysnij($arrOfFiles);
+$new = zasysnij([$newF]);
+
+$out = []; $i = 0; $j = 0;
+
+foreach($new as $kod) {
+    if( !in_array($kod, $out) ) { // nie ma go w out
+        if( !in_array($kod, $old) ) { // w starych też go nie ma
+            $out[] = $kod; // bieżemy
+            $i++;
+            if( $i == 1000 ) { // To dla wizualizacji postępu
+                $i = 0;
+                echo "\n" . ++$j;
+            }
+        }
+    }
+}
+
+//Zapisz wynik
+$h = fopen($outF, "w");
+foreach( $out as $kod ) {
+    fwrite($h, $kod . "\n");
+}
+fclose($h);
