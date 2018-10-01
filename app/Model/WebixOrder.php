9c8e947160a81495b921e8463a8245a6c30d1efb
dexter69 <dex@polskiekarty.pl>
2018-10-01 14:28:49 +0200

    Cd. podpolerowany kod

--- app/Model/WebixOrder.php
+++ app/Model/WebixOrder.php
@@ -8,16 +8,17 @@ class WebixOrder extends AppModel {
 
     public function getAllPrivateOrders( $idHandlowca = 0 ) {
 
-        if( $idHandlowca ) { // Mamy $id Handlowca - szukamy tylko dla tego Handlowca
-            $prywatne = $this->find('all', array(
-                'conditions' => array('WebixOrder.status' => 0, 'WebixOrder.user_id' => $idHandlowca)
-            ));
-        } else { // Szykamy wszystkich prywatnych
-            $prywatne = $this->find('all', array(
-                'conditions' => array('WebixOrder.status' => 0)
-            ));
-        }     
-        return $prywatne;
+        $searchParams = [
+            'fields' => ['id', 'user_id', 'nr', 'status'],
+            'conditions' => ['WebixOrder.status' => 0]
+        ];
+
+        if( $idHandlowca ) {
+        // Mamy $id Handlowca - szukamy tylko dla tego Handlowca => trzeba dorzucić warunek
+            $searchParams['conditions']['WebixOrder.user_id'] = $idHandlowca;
+        }
+
+        return $this->find('all', $searchParams);        
     }
 
 }
