c0d0a1bdb5041a541f1f0aaf5bc3dd3bf07fb9b3
dexter69 <dex@gmx.ie>
2018-10-10 07:36:02 +0200

    ...

--- app/webroot/webix/app/js/content/orders/privateOrders.js
+++ app/webroot/webix/app/js/content/orders/privateOrders.js
@@ -8,7 +8,8 @@ let privateOrders = {//,header:["Category",  {content:'selectFilter'}]
         { id:"index", header:"", sort:"int", adjust:true },
         { id:"id", header:"id", adjust:true },
         { id: "customerName", header: "Klient",  fillspace:true },                
-        { id: "creatorName", header: [{content:'selectFilter'} ] , width:105 },
+        { id: "creatorName", header: [{content:'serverSelectFilter'} ] , width:105 },
+        //{ id: "creatorName", header: [{content:'selectFilter'} ] , width:105 },
         { id: 'stop_day', header:"Termin", adjust: true }        
     ],
     scheme:{
@@ -17,7 +18,7 @@ let privateOrders = {//,header:["Category",  {content:'selectFilter'}]
     //data: prywatneZamowienia
     //data: gibon(prywatneZamowienia)   
     //url: "webixOrders/testData.json"
-    url: "webixOrders/privateOrders.json"    
+    url: "rest->webixOrders/privateOrders/0.json"    
 };
 
 function gibon( argument ) { return argument;}
