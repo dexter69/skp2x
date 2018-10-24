87a313f7acf9e3d9f4edd1d88b5295d995952283
dexter69 <dex@gmx.ie>
2018-10-24 22:13:33 +0200

    ...

--- app/webroot/webix/app/js/content/orders/orderDetail.js
+++ app/webroot/webix/app/js/content/orders/orderDetail.js
@@ -1,6 +1,12 @@
+let sample = [
+ 
+    { 'id': 12792, name: "Karta1", order_id:6829 }, 
+    { 'id': 12801, name: "Karta2", order_id:6829 }
+];
 let orderDetail = {
     id: "orderDetailListOfCards",
     view: "datatable",
+    orderId: 0,
     columns: [
         { id:"index", header:"", sort:"int", adjust:true },
         { id:"id", header:"id", adjust:true },
@@ -12,7 +18,16 @@ let orderDetail = {
         { 'id': 12801, name: "Karta2", order_id:6829 }
     ],
      */
-    url: "webixOrders/getOneOrderLight/6829.json",
+    //url: "webixOrders/getOneOrderLight/6829.json",
+    //data: sample,
+    url: function(){
+        let url = "webixOrders/getOneOrderLight.json";
+        console.log(orderDetail.orderId);
+        return webix.ajax(url).then(function(data){
+            let dane = data.json();              
+            return dane;                           
+        })        
+    },
     scheme:{
         $init:function(obj){ obj.index = this.count(); }
     },
@@ -20,16 +35,15 @@ let orderDetail = {
     //dataFeed: "costam.php"
 };
 
-let sample = [
-    { 'id': 12792, name: "Karta1", order_id:6829 }, 
-    { 'id': 12801, name: "Karta2", order_id:6829 }
-];
 
+
+/*
 webix.ready(function(){    
    $$("orderDetailListOfCards").bind( $$("privo")
    , function(slave, master){ return slave.order_id == master.WebixPrivateOrder_id;  }
     );
 });
+*/
 
 
 
