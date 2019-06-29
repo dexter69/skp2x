7a40d8a6c2c22c0ec3839245b482be586d85d1eb
dexter69 <dex@polskiekarty.pl>
2019-06-29 14:45:33 +0200

    Chwilowo preprosem łączymy pliki js

--- app/webroot/webix/app/js/app.js
+++ app/webroot/webix/app/js/app.js
@@ -1,3 +1,19 @@
+//@prepros-prepend ./content/customers/listOfCustomers.js
+//@prepros-prepend ./content/orders/addNewQuickOrder.js
+//@prepros-prepend ./content/customers/customerDetail.js
+//@prepros-prepend ./content/customers/customerPanel.js
+//@prepros-prepend ./content/orders/privateOrders/conf.js
+//@prepros-prepend ./content/orders/privateOrders/eventsHandlers.js
+//@prepros-prepend ./content/orders/theOrderDetail/listOfCards.js
+//@prepros-prepend ./content/orders/theOrderDetail/theOrderDetail.js
+//@prepros-prepend ./content/orders/privateOrders/listOfPrivateOrders.js
+//@prepros-prepend ./content/orders/managePrivateOrders.js                  
+//@prepros-prepend ./content/customers/manageCustomers.js 
+//@prepros-prepend ./layout/toolbarMenu.js
+//@prepros-prepend ./layout/mainToolbar.js
+//@prepros-prepend ./layout/leftSidebar.js                
+//@prepros-prepend ./layout/content.js
+
 /**
  * Główny plik js */
 webix.ready(function(){ //to ensure that your code is executed after the page is fully loaded
