a1a57d3b614a42e9cdec91c491e3cc362deb6102
dexter69 <dex@polskiekarty.pl>
2021-02-15 20:31:35 +0100

    Kwestia zdublowanych plików dalsze postępy

--- app/Skrypty/zduplikowane_pliki/znajdz.sql
+++ app/Skrypty/zduplikowane_pliki/znajdz.sql
@@ -9,3 +9,10 @@ SELECT `filename`, `role`, `filesize`, `created`, `id`, `uuidname`, `new-uuidnam
 FROM `uploads`
 WHERE `job_id`=0 AND `filesize` >= 7174470
 ORDER BY `filename` ASC, `filesize` DESC, `created` ASC
+
+-- THIRD APPROACH - more lepse -)
+-- Otrzymujemy jedną nazwę dla pliku na dysku; dla ew. potrzeb mamy też id kart, pod które te pliki są podpięte
+SELECT uploads.`id` AS Id_Pliku, uploads.`filename`, uploads.`filesize`, CONCAT(COALESCE(uploads.`uuidname`,''), COALESCE(uploads.`new-uuidname`,'')) AS lxname, cards_uploads.`card_id` AS Id_Karty
+FROM `uploads` JOIN `cards_uploads` ON uploads.`id` = `cards_uploads`.upload_id
+WHERE `job_id`=0 AND `filesize` >= 7174470
+ORDER BY `filename` ASC, `filesize` DESC, `created` ASC
