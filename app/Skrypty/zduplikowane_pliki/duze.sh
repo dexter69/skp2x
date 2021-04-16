ebd3a2429eb8128c1cda5b7d414599408d90c3f9
dexter69 <dex@polskiekarty.pl>
2021-04-16 13:33:03 +0200

    Przenoszenie i linkowanie duzych plików

--- app/Skrypty/zduplikowane_pliki/duze.sh
+++ app/Skrypty/zduplikowane_pliki/duze.sh
@@ -3,3 +3,11 @@
 # Przenieś 5 największych plików do folderu "jakiskatalog"
 
 mv `ls -S | head -5` /jakiskatalog
+
+# Przenieś i zlinkuj
+# Przenieś 50 największych plików do katalogu ....attachments/skp2
+# A następnie zlinkuj wszystkiete pliki do katalogu, gdzie SKP ma pliki (tak ze nadal będę dostępne)
+
+mv `ls -S | head -50` /var/www/forum.lan/attachments/skp2
+
+ln -s /var/www/forum.lan/attachments/skp2/* /var/www/skp/skp.lan/public_html/app/uploads
