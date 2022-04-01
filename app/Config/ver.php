18dc02c9084b805f68f481ac5085f771a35b3b5d
dexter69 <dex@polskiekarty.pl>
2022-04-01 15:47:02 +0200

    A jeszcze wersja

--- app/Config/ver.php
+++ app/Config/ver.php
@@ -7,4 +7,4 @@
 // To uzywamy w WebixHelper
 define('appVersion', '2.8.10');
 // A to w AppHelper
-define('appNumber', '2021-02-16_10-37'); // Just date and subversion
+define('appNumber', '2022-04-01_15-45'); // Just date and subversion
